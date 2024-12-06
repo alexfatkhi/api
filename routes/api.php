8a7f56c212bb2fa9d299842770896946f5796bd5
Syaifur Rohman <syaifurplus@gmail.com>
2024-12-06 13:21:10 +0700

    fix api. done

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MahasiswaController;
 
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
     Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
     Route::put('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'update']);
     Route::delete('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'destroy']);
